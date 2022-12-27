67848d05d1983d5d5a913caf1b9836d87ccc05ea
Johan Bautista <johan.bautista@cuatroochenta.com>
2022-12-28 00:46:33 +0100

    basic css card

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,21 +1,42 @@
-import { config } from "../../devdash_config";
+import { githubApiResponses } from "../../github_api_responses";
+import { isoToReadableDate } from "../../utils/isoToReadableDate";
+import styles from "./Dashboard.module.scss";
+import { ReactComponent as Check } from "./icons/check.svg";
+import { ReactComponent as Error } from "./icons/error.svg";
+import { ReactComponent as Lock } from "./icons/lock.svg";
+import { ReactComponent as Unlock } from "./icons/unlock.svg";
 
 export const Dashboard = () => {
-	// const widgets = config.widgets.map((widget) => <li key={widget.id}>{widget.repository_url}</li>);
 	const title = "DevDash_";
 
 	return (
 		<>
-			<header>
+			<header className={styles.container}>
 				<h2>{title}</h2>
 			</header>
-			<section>
-				{/* <ul>{widgets}</ul> */}
-				<ul>
-					{config.widgets.map((widget) => (
-						<li key={widget.id}>{widget.repository_url}</li>
-					))}
-				</ul>
+			<section className={styles.container}>
+				{githubApiResponses.map((widget) => (
+					<article className={styles.widget} key={widget.repositoryData.id}>
+						<header className={styles.widget__header}>
+							<a
+								className={styles.widget__title}
+								href={widget.repositoryData.html_url}
+								target="_blank"
+								title={`${widget.repositoryData.organization.login}/${widget.repositoryData.name}`}
+								rel="noreferrer"
+							>
+								{widget.repositoryData.organization.login}/{widget.repositoryData.name}
+							</a>
+							{widget.repositoryData.private ? <Lock /> : <Unlock />}
+						</header>
+						<p>Last update {isoToReadableDate(widget.repositoryData.updated_at)}</p>
+						{widget.CiStatus.workflow_runs.length > 0 && (
+							<div>
+								{widget.CiStatus.workflow_runs[0].status === "completed" ? <Check /> : <Error />}
+							</div>
+						)}
+					</article>
+				))}
 			</section>
 		</>
 	);
---
+++ src/utils/isoToReadableDate.tsx
@@ -0,0 +1,16 @@
+export const isoToReadableDate = (lastUpdate: string): string => {
+	const lastUpdateDate = new Date(lastUpdate);
+	const currentDate = new Date();
+	const diffTime = currentDate.getTime() - lastUpdateDate.getTime();
+	const diffDays = Math.round(diffTime / (1000 * 3600 * 24));
+
+	if (diffDays === 0) {
+		return "today";
+	}
+
+	if (diffDays > 30) {
+		return "more than a month ago";
+	}
+
+	return `${diffDays} days ago`;
+};
